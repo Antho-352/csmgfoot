978eddbf8cd80b9fee2124bb4ad75eddfffd92fc
Antho-352 <contact@anthonyrusso.fr>
2026-03-03 09:33:53 +0900

    feat: redirection non-www vers www.csmgfoot.fr

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://csmgfoot.fr',
+  site: 'https://www.csmgfoot.fr',
   output: 'static',
   adapter: node({ mode: 'standalone' }),
   integrations: [
